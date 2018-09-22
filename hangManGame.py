de2a6a6cbcf309f661c41c0c118212f6ea01e6ad
Laura_Git <laurama1007@gmail.com>
2018-09-22 18:35:40 +0200

    HangmanGameIn Python

--- hangManGame.py
+++ hangManGame.py
@@ -21,16 +21,14 @@ while True:
     # Say welcome to the players
     print("\n""This game is hosted by " + host.name + " and the contestant is " + guesser.name + "!" + "\n")
 
-
     # Choose a word
-    
     word = input(host.name + ", " + "Write a word: ")
     word = word.lower()
 
-    #clear screen
+    # clear screen
     os.system('clear')
 
-    #TODO: Show the underscores accroding to the length of the word 
+    # Show the underscores accroding to the length of the word 
     wordLength = word.__len__()
     progress = "*" * wordLength
         
@@ -40,10 +38,9 @@ while True:
     print("\t***********************************************")
     print("\n\n""Current State: " + progress + "\n")
 
-
-
-    #TODO: save guessed letters
+    # Save guessed letters
     guessed_letters = []
+
 
 
     errorCount = 0 
@@ -54,12 +51,12 @@ while True:
         guessed_letters.append(enterALetter.lower())
         print("The letters you have guessed: " + ', '.join(guessed_letters))
     
-        #Check if the word contains the guess letter
+        # Check if the word contains the guess letter
         if word.find(enterALetter) != -1:
             # Find the position, replace the underscore with the letter
             occurrences = word.find(enterALetter)
             indices = [i for i,  a in enumerate(word) if a == enterALetter]
-            #print(indices)
+            # Print(indices)??
             
             tmp_progress = list(progress)
             for index in indices:
@@ -81,6 +78,7 @@ while True:
             if errorCount == 9:
                 print("\n""You have reached the total missing times, Game Over!")
                 break
+                
     choice = input("Press q to quit the game. Press anything to start again.")
     
     if choice == "q":
